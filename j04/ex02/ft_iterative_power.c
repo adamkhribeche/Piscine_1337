99d323c6ac83d443d25a368837384affead0122e
adamkhribeche <khribechadam@gmail.com>
2019-04-18 04:10:35 +0100

    finish j04

---
+++ j04/ex02/ft_iterative_power.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_iterative_power.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nkhribec <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/02/17 14:38:55 by nkhribec          #+#    #+#             */
+/*   Updated: 2019/02/17 14:42:25 by nkhribec         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int		ft_iterative_power(int nb, int power)
+{
+	int result;
+
+	result = nb;
+	if (power < 0)
+		return (0);
+	if (power == 0)
+		return (1);
+	while (power != 1)
+	{
+		result *= nb;
+		power--;
+	}
+	return (result);
+}
---
+++ j04/ex03/ft_recursive_power.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_recursive_power.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nkhribec <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/02/17 14:47:11 by nkhribec          #+#    #+#             */
+/*   Updated: 2019/02/17 14:47:16 by nkhribec         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int		ft_recursive_power(int nb, int power)
+{
+	if (power < 0)
+		return (0);
+	if (power == 0)
+		return (1);
+	if (power == 1)
+		return (nb);
+	return (nb * ft_recursive_power(nb, power - 1));
+}
